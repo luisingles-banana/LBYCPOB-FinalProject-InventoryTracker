8371437007feeaa46b52c346fb13db8c10c9ac0b
Me <luis_ingles@dlsu.edu.ph>
2026-08-03 02:48:05 +0800

    Created Database.java as object

---
+++ src/main/java/ph.dlsu.lbycpob.inventorytracker/items/Inventory/Database.java
@@ -0,0 +1,13 @@
+package ph.dlsu.lbycpob.inventorytracker.items.Inventory;
+
+public class Database {
+    private String name;
+
+    public Database(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
