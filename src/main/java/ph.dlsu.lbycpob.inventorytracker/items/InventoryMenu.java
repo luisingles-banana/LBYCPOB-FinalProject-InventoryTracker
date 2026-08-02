02e2f8c02a77eee7ef59750b9be70b701db8d581
Me <luis_ingles@dlsu.edu.ph>
2026-08-03 02:05:11 +0800

    Created InventoryMenu Extending BaseMenu Displaying Initial Screen and Options for Inventory Tracker

--- src/main/java/ph.dlsu.lbycpob.inventorytracker/items/InventoryMenu.java
+++ src/main/java/ph.dlsu.lbycpob.inventorytracker/items/InventoryMenu.java
@@ -1,4 +1,37 @@
 package ph.dlsu.lbycpob.inventorytracker.items;
 
-public class InventoryMenu {
+public class InventoryMenu extends BaseMenu{
+    @Override
+    protected void printMenu() {
+        IO.println("""
+---------------------------------------------------
+              INVENTORY TRACKER
+---------------------------------------------------
+""");
+        displayAllDatabases();
+        IO.println("""
+    1. Enter Database
+    2. Add Database
+    3. Delete Database
+    0. Back to Main Menu
+                """);
+        IO.println("Choose an option: ");
+    }
+
+    @Override
+    protected boolean userChoice(int choice) {
+        switch (choice) {
+            case 1 -> enterDatabase();
+            case 2 -> addDatabase();
+            case 3 -> deleteDatabase();
+            case 0 -> { return false; }
+            default -> IO.println("Invalid option, try again.");
+        }
+        return true;
+    }
+
+    private void displayAllDatabases() { /* TODO */ }
+    private void enterDatabase() { /* TODO */ }
+    private void addDatabase() { /* TODO */ }
+    private void deleteDatabase() { /* TODO */ }
 }
