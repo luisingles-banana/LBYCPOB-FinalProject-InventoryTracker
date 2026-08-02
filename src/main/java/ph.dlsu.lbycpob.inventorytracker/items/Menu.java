4b4b1048b6fc43c456e5e393a754cce73f150ae1
Me <luis_ingles@dlsu.edu.ph>
2026-08-03 02:10:54 +0800

    Modified Main Menu to extend BaseMenu

--- src/main/java/ph.dlsu.lbycpob.inventorytracker/items/Menu.java
+++ src/main/java/ph.dlsu.lbycpob.inventorytracker/items/Menu.java
@@ -1,42 +1,31 @@
 package ph.dlsu.lbycpob.inventorytracker.items;
-import java.util.Scanner;
 
-public class Menu {
-    private Scanner scanner = new Scanner(System.in);
+public class Menu extends BaseMenu{
 
-    public void run() {
-        boolean running = true;
-        while (running) {
-            mainMenu();
-            int choice = readChoice();
-            switch (choice) {
-                case 1 -> IO.println("Option 1");
-                case 2 -> IO.println("Option 2");
-                case 0 -> running = false;
-                default -> IO.println("Invalid option, try again.");
-            }
-        }
-        IO.println("Goodbye!");
-    }
-
-    public void mainMenu() {
+    @Override
+    protected void printMenu() {
         IO.println("""
 ---------------------------------------------------
       FINANCE & MATERIALS INVENTORY TRACKER
 ---------------------------------------------------
     1. Finance Tracker
     2. Inventory Tracker
-    3. Exit
+    0. Exit
                 """);
         IO.println("Choose an option: ");
     }
 
-    // Helper Function
-    private int readChoice() {
-        try {
-            return Integer.parseInt(scanner.nextLine().trim());
-        } catch (NumberFormatException e) {
-            return -1;
+    @Override
+    protected boolean userChoice(int choice) {
+        switch (choice) {
+            case 1 -> enterFinance();
+            case 2 -> enterInventory();
+            case 0 -> { return false; }
+            default -> IO.println("Invalid option, try again.");
         }
+        return true;
     }
+
+    private void enterInventory() { /* TODO */ }
+    private void enterFinance() { /* TODO */ }
 }
