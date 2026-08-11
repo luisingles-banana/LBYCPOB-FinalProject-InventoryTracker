5d65bac817a4edf118b27634530938adbe6449d5
Luck Orville Mortel <luck_mortel@dlsu.edu.ph>
2026-08-11 20:49:34 +0800

    Add main menu navigation

--- src/main/java/ph/dlsu/lbycpob/inventorytracker/items/Menu/Menu.java
+++ src/main/java/ph/dlsu/lbycpob/inventorytracker/items/Menu/Menu.java
@@ -10,6 +10,7 @@ public class Menu extends BaseMenu{
 ---------------------------------------------------
     1. Finance Tracker
     2. Inventory Tracker
+    3. SOP & Emergency Protocols
     0. Exit
                 """);
         IO.println("Choose an option: ");
@@ -20,6 +21,7 @@ public class Menu extends BaseMenu{
         switch (choice) {
             case 1 -> enterFinance();
             case 2 -> enterInventory();
+            case 3 -> enterSOP();
             case 0 -> { return false; }
             default -> IO.println("Invalid option, try again.");
         }
@@ -27,5 +29,6 @@ public class Menu extends BaseMenu{
     }
 
     private void enterInventory() { new InventoryMenu().run(); }
+    private void enterSOP() { new SOPMenu().run(); }
     private void enterFinance() { /* TODO */ }
 }
